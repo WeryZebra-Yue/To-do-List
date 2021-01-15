0cb5ad8fa3c3e3763c4141090ca3ee062e355161
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2021-01-15 13:22:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let landing = document.querySelector("#landingid");
 let audio = document.querySelector('audio');
 let audio_button = document.querySelector("#audiobutton");
 let property = document.querySelector(".property");
-let i = 1;
 let Menu = document.querySelector("#Menu");
 let portfoliodoc = document.querySelector("#portfolio");
 let portfoliodetails = document.querySelector("portfolio");
@@ -17,6 +16,7 @@ let Functiondoc = document.querySelector("#function");
 let Functiodetails = document.querySelector("Functions");
 let Notesdoc = document.querySelector("#notes");
 let Notesdetails = document.querySelector("Notes");
+let i = 0;
 
 audio_button.addEventListener('click', () => {
     var button = i % 2 != 0 ? 'icons8-speaker-100.png' : 'icons8-mute-100.png';
@@ -24,11 +24,16 @@ audio_button.addEventListener('click', () => {
     i % 2 != 0 ? audio.play() : audio.pause();
     i++
 });
+var _0x5ce7 = ['innerHTML', '3CTGUBP', '1545201onfkOe', '1305573XMhYnp', '1lZpEMF', '968849NKTdrh', '429841xdRSIx', '-->\x20Made\x20by\x20Ayush\x20Master.', '1cFTyyw', '261682plyleX', '706235PUAARO', '1KueBkK', '593740bsnvcv'];
+var _0x490d = function(_0x38c6dd, _0xe1020c) { _0x38c6dd = _0x38c6dd - 0xb2; var _0x5ce767 = _0x5ce7[_0x38c6dd]; return _0x5ce767; };
+var _0x678940 = _0x490d;
+(function(_0x341229, _0x5ea376) { var _0x273a08 = _0x490d; while (!![]) { try { var _0x4c072b = parseInt(_0x273a08(0xbc)) + parseInt(_0x273a08(0xb2)) * -parseInt(_0x273a08(0xbb)) + -parseInt(_0x273a08(0xb6)) + parseInt(_0x273a08(0xb4)) * parseInt(_0x273a08(0xb3)) + parseInt(_0x273a08(0xb9)) * parseInt(_0x273a08(0xbe)) + -parseInt(_0x273a08(0xb5)) * -parseInt(_0x273a08(0xb8)) + -parseInt(_0x273a08(0xba)); if (_0x4c072b === _0x5ea376) break;
+            else _0x341229['push'](_0x341229['shift']()); } catch (_0x107d62) { _0x341229['push'](_0x341229['shift']()); } } }(_0x5ce7, 0xed49b), property[_0x678940(0xbd)] = _0x678940(0xb7));
 Menu.addEventListener('click', () => {
     landing.classList.toggle('landinganime');
     Menu.classList.toggle("menu_anime1");
 
-})
+});
 let Name = prompt("Enter Your Name : ");
 change.innerHTML = Name ? Name + '&apos;s To-Do List' : change.innerHTML;
 submit.addEventListener('click', (e) => {
